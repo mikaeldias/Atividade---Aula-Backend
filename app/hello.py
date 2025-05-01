1f7b07358c8f9f646a25ea96cdab800a3717bca6
mikaeldias <165174397+MikaelDiasS10@users.noreply.github.com>
2025-05-01 13:40:48 -0300

    Melhorias na lógica do projeto

--- app/hello.py
+++ app/hello.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, flash, redirect, url_for
 from flask import render_template
 
 app = Flask(__name__)
+app.secret_key = 'Mikael_é_fodaaaaa'
 
 user = ['Mikael', 'Yasmin', 'Matheus', 'Claudia', 'Robson']
 password = ['123', '456', '789', '1011', '1213']
-
 @app.route("/", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -15,9 +15,9 @@ def login():
             logado = True
             return render_template('usuario.html', usuario = usuario_verificacao, logado = logado)
         else:
-            logado = False
-            return render_template('usuario.html',  usuario = usuario_verificacao)
+            flash('Senha incorreta. Tente novamente.', 'warning')
+            return redirect(url_for('login')) # retorna a url correspondente da função
     return render_template('index.html')
-@app.route('/login')
+@app.route('/usuario')
 def telaInicial():
     return render_template('usuario.html')
